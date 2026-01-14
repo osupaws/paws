6f54ccfac76f1f944d80b02b1404782548a116fa
Shosha <55537219+shsh-x@users.noreply.github.com>
2026-01-14 19:13:37 +0300

    Sidebar removed

--- frontend/src/main/windows/main/main.window.ts
+++ frontend/src/main/windows/main/main.window.ts
@@ -7,10 +7,8 @@ import { join } from "path";
 export const createMainWindow = (
   splashWindow: BrowserWindow,
 ): BrowserWindow => {
-  const isCompact: boolean = store.get("isCompact");
-
   const mainWindow = new BrowserWindow({
-    width: isCompact ? 672 : 1200,
+    width: 640,
     height: 800,
     resizable: false,
     frame: false,
